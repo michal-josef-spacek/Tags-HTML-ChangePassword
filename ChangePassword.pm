a7d449b9eaa70a128c5e320898c8ab620fe9178b
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-02-08 22:36:03 +0100

    Prepare for 0.03 version

--- ChangePassword.pm
+++ ChangePassword.pm
@@ -12,7 +12,7 @@ use Tags::HTML::Messages;
 
 Readonly::Array our @FORM_METHODS => qw(post get);
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -605,6 +605,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
